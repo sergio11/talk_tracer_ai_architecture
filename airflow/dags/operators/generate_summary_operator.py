9964dd38a9ccced9666cc6ac97a3d62f0a31737f
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-11-29 19:53:17 +0100

    Implement several improvements in Generate Summary operator

--- airflow/dags/operators/generate_summary_operator.py
+++ airflow/dags/operators/generate_summary_operator.py
@@ -5,6 +5,10 @@ from transformers import pipeline
 
 class GenerateSummaryOperator(BaseCustomOperator):
     
+    """
+    Operator responsible for generating a summary based on meeting transcribed text
+    using a summarization model and storing it in the BSON document in MongoDB.
+    """
     @apply_defaults
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -59,10 +63,15 @@ class GenerateSummaryOperator(BaseCustomOperator):
         # Set the maximum length of the summary based on the length of the input text
         max_summary_length = max(30, min(230, int(len(transcribed_text) * 0.5)))
 
+        self._log_to_mongodb(f"Generating summary using the summarization model...", context, "INFO")
         # Generate the summary using the Hugging Face summary model with the maximum length set dynamically
         summary = summarizer(transcribed_text, max_length=max_summary_length, min_length=30, do_sample=False)[0]['summary_text']
 
+        self._log_to_mongodb(f"Saving the summary to the BSON document...", context, "INFO")
         # Save the summary to the BSON document
         self._save_summary_to_bson(context, meeting_id, summary)
 
+        self._log_to_mongodb(f"Execution of GenerateSummaryOperator completed.", context, "INFO")
+
         return {"meeting_id": str(meeting_id)}
+
