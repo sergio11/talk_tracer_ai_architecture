5177de691a72e9d0f876ce5eec9476f13a0ec186
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-11-29 19:47:35 +0100

    Implement Natural Language Proccessing Operator

--- airflow/dags/talk_tracer_dag.py
+++ airflow/dags/talk_tracer_dag.py
@@ -16,8 +16,8 @@ with DAG('talk_tracer_dag', default_args=default_args, default_view="graph", sch
     # Import the necessary operators from external modules
     operators_module = importlib.import_module('operators.video_transcription_operator')
     VideoTranscriptionOperator = operators_module.VideoTranscriptionOperator
-    operators_module = importlib.import_module('operators.transcription_language_detection_operator')
-    TranscriptionLanguageDetectionOperator = operators_module.TranscriptionLanguageDetectionOperator
+    operators_module = importlib.import_module('operators.natural_language_proccessing_operator')
+    NaturalLanguageProccessingOperator = operators_module.NaturalLanguageProccessingOperator
     operators_module = importlib.import_module('operators.transcription_translation_operator')
     TranscriptionTranslationOperator = operators_module.TranscriptionTranslationOperator
     operators_module = importlib.import_module('operators.generate_summary_operator')
@@ -28,6 +28,17 @@ with DAG('talk_tracer_dag', default_args=default_args, default_view="graph", sch
     # Task to transcribe the video and store the transcription
     video_transcription_task = VideoTranscriptionOperator(
         task_id='video_transcription_task',
+        mongo_uri=os.environ.get("MONGO_URI"),
+        mongo_db=os.environ.get("MONGO_DB"),
+        mongo_db_collection=os.environ.get("MONGO_DB_COLLECTION"),
+        minio_endpoint=os.environ.get("MINIO_ENDPOINT"),
+        minio_access_key=os.environ.get("MINIO_ACCESS_KEY"),
+        minio_secret_key=os.environ.get("MINIO_SECRET_KEY"),
+        minio_bucket_name=os.environ.get("MINIO_BUCKET_NAME")
+    )
+
+    nlp_task = NaturalLanguageProccessingOperator(
+        task_id='nlp_task',
         mongo_uri=os.environ.get("MONGO_URI"),
         mongo_db=os.environ.get("MONGO_DB"),
         mongo_db_collection=os.environ.get("MONGO_DB_COLLECTION"),
@@ -62,4 +73,4 @@ with DAG('talk_tracer_dag', default_args=default_args, default_view="graph", sch
     )
 
     # Define task dependencies by chaining the tasks in sequence
-    video_transcription_task >> translation_task >> summary_task
+    video_transcription_task >> nlp_task >> translation_task >> summary_task
