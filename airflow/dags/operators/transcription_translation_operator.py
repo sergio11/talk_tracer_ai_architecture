cee1a648b8cf856da0fa78782dedb49ca3669a8f
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-11-30 21:06:28 +0100

    Implement several improvements in transcription translation operator

--- airflow/dags/operators/transcription_translation_operator.py
+++ airflow/dags/operators/transcription_translation_operator.py
@@ -30,14 +30,15 @@ class TranscriptionTranslationOperator(BaseCustomOperator):
         translation = translator.translate(text, dest=target_language)
         return translation.text
     
-    def _update_translations_in_mongodb(self, context, meeting_id, translations):
+    def _update_translations_in_mongodb(self, context, meeting_id, transcription_translations, summary_translations):
         """
         Updates the translations in the MongoDB document corresponding to the meeting_id.
 
         Args:
         - context (dict): The execution context.
         - meeting_id (str): The ID of the meeting/document in MongoDB.
-        - translations (dict): A dictionary containing translations for different languages.
+        - transcription_translations (dict): A dictionary containing translations for different languages.
+        - summary_translations(dict): A dictionary containing translations for different languages.
 
         The 'translations' dictionary should have the structure:
         {
@@ -49,13 +50,20 @@ class TranscriptionTranslationOperator(BaseCustomOperator):
         collection = self._get_mongodb_collection()
         update_result = collection.update_one(
             {"_id": ObjectId(meeting_id)},
-            {"$set": {"translations": translations}}
+            {
+                "$set": {
+                    "transcription_translations": transcription_translations,
+                    "summary_translations": summary_translations
+                }
+            }
         )
 
         if update_result.modified_count == 1:
             self._log_to_mongodb(f"Updated translations for document with meeting_id {meeting_id} in MongoDB", context, "INFO")
         else:
-            self._log_to_mongodb(f"Document with meeting_id {meeting_id} not updated with translations in MongoDB", context, "WARNING")
+            error_message = f"Document with meeting_id {meeting_id} not updated with translations in MongoDB"
+            self._log_to_mongodb(error_message, context, "WARNING")
+            raise Exception(error_message)
 
     def execute(self, context):
         self._log_to_mongodb(f"Starting execution of TranscriptionTranslationOperator", context, "INFO")
@@ -64,17 +72,27 @@ class TranscriptionTranslationOperator(BaseCustomOperator):
         meeting_id = context['dag_run'].conf.get('meeting_id')
         self._log_to_mongodb(f"Received meeting_id: {meeting_id}", context, "INFO")
 
-        transcribed_text = self._get_transcribed_text_from_context(context, meeting_id)
+        meeting_info = self._get_meeting_info(context, meeting_id)
+        transcribed_text = self._get_transcribed_text_from_meeting_info(context, meeting_info)
+        original_language = self._get_language_from_meeting_info(context, meeting_info)
+        summary = self._get_summary_from_meeting_info(context, meeting_info)
 
-        # Translate the transcribed text for each target language
+        # Translate the transcribed text and summary for each target language except the original language
         translated_texts = {}
+        translated_summaries = {}
         for language in self.target_languages:
-            translated_text = self._translate_text(transcribed_text, language)
-            translated_texts[language] = translated_text
-            self._log_to_mongodb(f"Translated text to {language} completed", context, "INFO")
+            if language != original_language:
+                translated_text = self._translate_text(transcribed_text, language)
+                translated_texts[language] = translated_text
+                self._log_to_mongodb(f"Translated text to {language} completed", context, "INFO")
+
+                translated_summary = self._translate_text(summary, language)
+                translated_summaries[language] = translated_summary
+                self._log_to_mongodb(f"Translated summary to {language} completed", context, "INFO")
 
         # Update the translations in the MongoDB document
-        self._update_translations_in_mongodb(context, meeting_id, translated_texts)
+        self._update_translations_in_mongodb(context, meeting_id, translated_texts, translated_summaries)
         self._log_to_mongodb(f"Updated translations in MongoDB for meeting_id: {meeting_id}", context, "INFO")
-        
+
         return {"meeting_id": str(meeting_id)}
+        
