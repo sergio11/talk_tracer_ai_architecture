ca1048a326c5598c050af925376405500391a5d5
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2023-12-03 15:04:10 +0100

    Implement several improvements in Talk Tracer DAG definition

--- airflow/dags/talk_tracer_dag.py
+++ airflow/dags/talk_tracer_dag.py
@@ -14,20 +14,21 @@ default_args = {
 # Create the DAG with the specified default arguments
 with DAG('talk_tracer_dag', default_args=default_args, default_view="graph", schedule_interval=None, catchup=False) as dag:
     # Import the necessary operators from external modules
-    operators_module = importlib.import_module('operators.video_transcription_operator')
-    VideoTranscriptionOperator = operators_module.VideoTranscriptionOperator
+    operators_module = importlib.import_module('operators.transcription_operator')
+    TranscriptionOperator = operators_module.TranscriptionOperator
     operators_module = importlib.import_module('operators.natural_language_proccessing_operator')
     NaturalLanguageProccessingOperator = operators_module.NaturalLanguageProccessingOperator
     operators_module = importlib.import_module('operators.generate_summary_operator')
     GenerateSummaryOperator = operators_module.GenerateSummaryOperator
-    operators_module = importlib.import_module('operators.transcription_translation_operator')
-    TranscriptionTranslationOperator = operators_module.TranscriptionTranslationOperator
+    operators_module = importlib.import_module('operators.translation_operator')
+    TranslationOperator = operators_module.TranslationOperator
 
     # Define the task instances for each operator
 
-    # Task to transcribe the video and store the transcription
-    video_transcription_task = VideoTranscriptionOperator(
-        task_id='video_transcription_task',
+    # Task to transcribe the audio and store the transcription
+    transcription_task = TranscriptionOperator(
+        task_id='transcription_task',
+        audio_segment_duration=os.environ.get("AUDIO_SEGMENT_DURATION"),
         mongo_uri=os.environ.get("MONGO_URI"),
         mongo_db=os.environ.get("MONGO_DB"),
         mongo_db_collection=os.environ.get("MONGO_DB_COLLECTION"),
@@ -49,9 +50,9 @@ with DAG('talk_tracer_dag', default_args=default_args, default_view="graph", sch
         minio_bucket_name=os.environ.get("MINIO_BUCKET_NAME")
     )
 
-    # Task to generate a summary of the transcription and store the summary
+    # Task to generate a summary of the transcription and store it
     summary_task = GenerateSummaryOperator(
-        task_id='openai_summary_task',
+        task_id='summary_task',
         mongo_uri=os.environ.get("MONGO_URI"),
         mongo_db=os.environ.get("MONGO_DB"),
         mongo_db_collection=os.environ.get("MONGO_DB_COLLECTION"),
@@ -61,8 +62,8 @@ with DAG('talk_tracer_dag', default_args=default_args, default_view="graph", sch
         minio_bucket_name=os.environ.get("MINIO_BUCKET_NAME")
     )
 
-    # Task to translate the transcription into another language and store the translation
-    translation_task = TranscriptionTranslationOperator(
+    # Task to translate the transcription and summary into another language and store the translation
+    translation_task = TranslationOperator(
         task_id='translation_task',
         mongo_uri=os.environ.get("MONGO_URI"),
         mongo_db=os.environ.get("MONGO_DB"),
@@ -71,8 +72,8 @@ with DAG('talk_tracer_dag', default_args=default_args, default_view="graph", sch
         minio_access_key=os.environ.get("MINIO_ACCESS_KEY"),
         minio_secret_key=os.environ.get("MINIO_SECRET_KEY"),
         minio_bucket_name=os.environ.get("MINIO_BUCKET_NAME"),
-        target_languages=os.environ.get("TARGET_LANGUAGES").split(",")
+        target_languages=os.environ.get("TRANSLATION_TARGET_LANGUAGE").split(",")
     )
 
     # Define task dependencies by chaining the tasks in sequence
-    video_transcription_task >> nlp_task >> summary_task  >> translation_task 
+    transcription_task >> nlp_task >> summary_task >> translation_task 
